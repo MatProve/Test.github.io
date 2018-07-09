34333177faf85769f799bda088c7aee5e4a4d6af
MatProve <40834676+MatProve@users.noreply.github.com>
2018-07-09 14:58:38 +0200

    Add files via upload

---
+++ data/BDD_Cutt_encours_11.js
@@ -0,0 +1,14 @@
+var json_BDD_Cutt_encours_11 = {
+"type": "FeatureCollection",
+"name": "BDD_Cutt_encours_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nom_tir": "68CN067-102", "Num_vol�e": "68CN067", "Num_trou": 102.0, "Y(q)": 79.68, "IJ": 13.92, "Qualite_mesure": "Jaune", "Prof_tir": 15.3, "x": 635154.0, "y": 57436.0, "z": 420.2, "coord_brutes": "015;635154;057436;420,2;" }, "geometry": { "type": "Point", "coordinates": [ 2.766374680736207, 42.815933137107983 ] } },
+{ "type": "Feature", "properties": { "Nom_tir": "68CN067-103", "Num_vol�e": "68CN067", "Num_trou": 103.0, "Y(q)": 79.05, "IJ": 17.29, "Qualite_mesure": "Jaune � Traiter", "Prof_tir": 15.3, "x": 635163.0, "y": 57442.0, "z": 420.9, "coord_brutes": "016;635163;057442;420,9;" }, "geometry": { "type": "Point", "coordinates": [ 2.76648509166046, 42.81598671781714 ] } },
+{ "type": "Feature", "properties": { "Nom_tir": "68CN067-105", "Num_vol�e": "68CN067", "Num_trou": 105.0, "Y(q)": 69.67, "IJ": 26.54, "Qualite_mesure": "St�rile", "Prof_tir": 15.3, "x": 635168.0, "y": 57445.0, "z": 420.5, "coord_brutes": "017;635168;057445;420,5;" }, "geometry": { "type": "Point", "coordinates": [ 2.766546409921929, 42.816013484693613 ] } },
+{ "type": "Feature", "properties": { "Nom_tir": "68CN067-107", "Num_vol�e": "68CN067", "Num_trou": 107.0, "Y(q)": 70.25, "IJ": 25.73, "Qualite_mesure": "St�rile", "Prof_tir": 15.3, "x": 635175.0, "y": 57451.0, "z": 423.6, "coord_brutes": "018;635175;057451;423,6;" }, "geometry": { "type": "Point", "coordinates": [ 2.766632370219866, 42.81606715900611 ] } },
+{ "type": "Feature", "properties": { "Nom_tir": "68CN068-102", "Num_vol�e": "68CN068", "Num_trou": 102.0, "Y(q)": 88.12, "IJ": 7.64, "Qualite_mesure": "ExtraBlanc", "Prof_tir": 15.3, "x": 634923.0, "y": 57375.0, "z": 396.2, "coord_brutes": "012;634923;057375;396,2;" }, "geometry": { "type": "Point", "coordinates": [ 2.763546747558395, 42.815394900102596 ] } },
+{ "type": "Feature", "properties": { "Nom_tir": "68CN068-103", "Num_vol�e": "68CN068", "Num_trou": 103.0, "Y(q)": 85.54, "IJ": 10.17, "Qualite_mesure": "Blanc", "Prof_tir": 15.3, "x": 634926.0, "y": 57376.0, "z": 395.0, "coord_brutes": "013;634926;057376;395,0;" }, "geometry": { "type": "Point", "coordinates": [ 2.763583486780193, 42.815403760867099 ] } },
+{ "type": "Feature", "properties": { "Nom_tir": "68CN068-104", "Num_vol�e": "68CN068", "Num_trou": 104.0, "Y(q)": 88.15, "IJ": 7.28, "Qualite_mesure": "ExtraBlanc", "Prof_tir": 15.3, "x": 634931.0, "y": 57380.0, "z": 393.8, "coord_brutes": "014;634931;057380;393,8;" }, "geometry": { "type": "Point", "coordinates": [ 2.763644866409229, 42.815439529743841 ] } }
+]
+}
---
+++ data/Volees_en_cours_10.js
@@ -0,0 +1,9 @@
+var json_Volees_en_cours_10 = {
+"type": "FeatureCollection",
+"name": "Volees_en_cours_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "group": "68CN068", "begin": "4", "end": "7", "Ordre": "7", "Nom_tir": "68CN068", "Y(q)": 87.27, "IJ": 8.36, "Qualitee": "Blanc\/B3", "Date_traca": null, "TONTAL (=v": null, "X": null, "Y": null, "Z": null, "Coord_comp": "man", "Prof_tir": null, "field_13": null, "field_14": null, "field_15": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 2.763485557692665, 42.815386132410033 ], [ 2.763657218238506, 42.815457484010324 ], [ 2.763680262965338, 42.815431294348507 ], [ 2.763506094179223, 42.81535095168006 ], [ 2.763485557692665, 42.815386132410033 ] ] ] } },
+{ "type": "Feature", "properties": { "group": "68CN067", "begin": "0", "end": "3", "Ordre": "1", "Nom_tir": "68CN067", "Y(q)": 74.65851, "IJ": 20.8689, "Qualitee": "Jaune\/Sterile", "Date_traca": null, "TONTAL (=v": null, "X": "635168", "Y": "57455", "Z": "374", "Coord_comp": "031;635168;057455;374;", "Prof_tir": null, "field_13": null, "field_14": null, "field_15": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 2.766374744386972, 42.815942137519961 ], [ 2.766547046686988, 42.816103488814591 ], [ 2.766640967500642, 42.816055425181986 ], [ 2.766394242785101, 42.815898679625178 ], [ 2.766374744386972, 42.815942137519961 ] ] ] } }
+]
+}
